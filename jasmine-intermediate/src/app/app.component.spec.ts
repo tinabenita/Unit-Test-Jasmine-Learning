a6f5302144b241cbb36a715d39746901c6378ff5
tinabenita <tina1717rego@gmail.com>
2025-03-20 12:21:00 +0530

    feat: flush()
    used to execute all pending timers

--- jasmine-intermediate/src/app/app.component.spec.ts
+++ jasmine-intermediate/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -8,23 +8,25 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  xit('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'jasmine-intermediate' title`, () => {
+  xit(`should have the 'jasmine-intermediate' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('jasmine-intermediate');
   });
 
-  it('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jasmine-intermediate');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, jasmine-intermediate'
+    );
   });
 
   describe('this test', () => {
@@ -40,7 +42,26 @@ describe('AppComponent', () => {
         expect(flag).toBe(false);
         tick(50);
         expect(flag).toBe(true);
-      }),
+      })
+    );
+  });
+
+  describe('this test is a flush demo', () => {
+    it(
+      'looks async but is synchronous',
+      <any>fakeAsync((): void => {
+        let flag = false;
+        setTimeout(() => {
+          flag = true;
+        }, 100);
+        expect(flag).toBe(false);
+        flush();
+        /*
+        The tick calls are replaced with a single flush call.
+        The flush function advances the virtual clock and executes all pending timers, ensuring that the setTimeout callback is executed.
+        */
+        expect(flag).toBe(true);
+      })
     );
   });
 });
